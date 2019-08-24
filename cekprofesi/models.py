b9072d3d9dc5d62e7f78e87c62f8e6b737f4d2ae
inayah28 <34393073+inayah28@users.noreply.github.com>
2019-08-24 18:40:03 +0700

    Update models.py

--- cekprofesi/models.py
+++ cekprofesi/models.py
@@ -10,7 +10,7 @@ class Matkul(models.Model):
 class Profesi_Matkul(models.Model):
     profesi = models.CharField(max_length=50, primary_key=True)
     kdmk = models.ForeignKey(Matkul, blank=True)
-    presentase = models.CharField(max_length=2) # --> harusnya 4 kalo formatnya 100%
+    presentase = models.CharField(max_length=4) # --> harusnya 4 kalo formatnya 100%
 
     def __str__(self):
         return "{}".format( self.profesi)
@@ -22,23 +22,3 @@ class Profesi(models.Model):
     def __str__(self):
         return "{}".format(self.profesi)
 
-class Matkur(models.Model):
-    kdmkr = models.CharField(max_length=9, primary_key=True)
-    matkur = models.CharField(max_length=50)
-    semester = models.CharField(max_length=1)
-    def __str__(self):
-        return "{}".format(self.matkur)
-
-
-
-class Profesi_Matkur(models.Model):
-    profesi = models.CharField(max_length=50, primary_key=True)
-    kdmkr = models.ForeignKey(Matkur, blank=True)
-    #matkur = models.CharField(max_length=50)
-    #semester = models.CharField(max_length=1)
-    presentase = models.CharField(max_length=2)
-    def __str__(self):
-        return "{}".format(self.profesi)
-
-    
-    
