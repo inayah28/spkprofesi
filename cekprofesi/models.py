88b8ac65c0e3f59d6a8e6ac4b2782f64cf920019
inayah28 <34393073+inayah28@users.noreply.github.com>
2019-08-28 15:11:29 +0700

    Update models.py

--- cekprofesi/models.py
+++ cekprofesi/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 
 class Matkul(models.Model):
-    kdmk = models.CharField(max_length=9, primary_key=True)
+    kdmk = models.CharField(max_length=9, primary_key=True, unique=True)
     matkul = models.CharField(max_length=9)
     semester = models.CharField(max_length=1)
     def __str__(self):
         return "{}".format(self.kdmk)
 
-class Profesi_Matkul(models.Model):
-    profesi = models.CharField(max_length=50, primary_key=True)
-    kdmk = models.ForeignKey(Matkul, blank=True)
+class ProfesiMatkul(models.Model):
+    profesi = models.CharField(max_length=25, default="")
+    kdmk = models.ForeignKey(Matkul,to_field="kdmk", db_column="kdmk")
     presentase = models.CharField(max_length=4) # --> harusnya 4 kalo formatnya 100%
 
     def __str__(self):
-        return "{}".format( self.profesi)
+        return "{}.{}".format(self.id,self.profesi)
 
 class Profesi(models.Model):
     kdprofesi = models.CharField(max_length=9, primary_key=True)
